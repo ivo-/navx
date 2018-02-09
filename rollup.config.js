da77e9e5b4c2324cd31369a968ea0b7122c25ef8
ivo- <ivailo.nikolaev.hristov@gmail.com>
2018-02-10 00:56:06 +0200

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,6 @@ import replace from 'rollup-plugin-replace';
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
 
-// https://github.com/reactjs/redux/blob/master/rollup.config.js
-// do i need to prefis rollup ./node_modules/.bin/rollup
 const env = process.env.NODE_ENV;
 const config = {
   input: 'src/index.js',
@@ -13,33 +11,27 @@ const config = {
       throwOnError: true,
       exclude: 'node_modules/**',
     }),
+    resolve({
+      jsnext: true,
+    }),
+    babel({
+      plugins: ['external-helpers'],
+      exclude: 'node_modules/**',
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env),
+    }),
   ],
 };
 
 if (env === 'es' || env === 'cjs') {
   config.output = { format: env };
-  config.plugins.push(babel({
-    plugins: ['external-helpers'],
-    exclude: 'node_modules/**',
-  }));
 } else {
   config.output = {
     file: '.bundle.js',
     format: 'umd',
     name: 'myBundle',
   };
-  config.plugins.push(
-    resolve({
-      jsnext: true,
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: ['external-helpers'],
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(env),
-    })
-  );
 }
 
 export default config;
